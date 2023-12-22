e7ca11e373a402a6b64e64ed20a5fa6a0974f5d7
bowen-anth <bowen.anth@gmail.com>
2023-12-22 11:14:26 -0700

    added navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Nav = () => {
+    return (
+        <>
+            <nav className="nav">
+                <img src="../public/globe.svg" />
+                <span className="nav-title"><span className="nav-title-line-height-adjuster">my travel journal.</span></span>
+            </nav>
+        </>
+    )
+}
+
+export default Nav
