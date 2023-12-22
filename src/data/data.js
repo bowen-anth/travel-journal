6209fed1bc2dcbad8c1acb1ca30984cbfcc8d90b
bowen-anth <bowen.anth@gmail.com>
2023-12-22 10:33:35 -0700

    added info to data file

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2011",
+        endDate: "24 Jan, 2011",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Belize",
+        location: "Belize",
+        googleMapsUrl: "https://maps.app.goo.gl/5mMbTBQWHnmyfJ9H9",
+        startDate: "05 Nov, 2013",
+        endDate: "12 Nov, 2013",
+        description: "Belize is a nation on the eastern coast of Central America, with Caribbean Sea shorelines to the east and dense jungle to the west.",
+        imageUrl: "https://unsplash.com/photos/brown-wooden-beach-chairs-near-body-of-water-during-daytime-erKKEmRLuV0"
+    },
+    {
+        title: "Spring Lake, Michigan",
+        location: "Spring Lake, Michigan",
+        googleMapsUrl: "https://maps.app.goo.gl/aQHd35yQ9kL3PXNUA",
+        startDate: "01 Jan, 2000",
+        endDate: "01 Jan, 2002",
+        description: "My hometown! Spring Lake is a village in Ottawa County in the U.S. state of Michigan. The population was 2,497 at the 2020 census.",
+        imageUrl: "https://www.lakescientist.com/wp-content/uploads/2011/04/spring-lake-michigan.jpg"
+    }
+]
